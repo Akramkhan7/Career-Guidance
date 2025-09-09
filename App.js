96adb419273405f3e8746cfcd466fe173d620f82
Akramkhan7 <m.akramkhan6690@gmail.com>
2025-09-09 16:36:21 +0530

    done

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const session = require("express-session");
 const connectDB = require("./config/db");
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
