73e9260a1a759a71cdec889be0bf756a19e915c1
AbhiGurjar <abhishekbainsla190@gmail.com>
2025-09-21 22:25:06 +0530

    routes setup

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const connectDB = require("./config/db");
+connectDB();
 // const routes = require('./routes/userRoutes')
 const fs = require('fs');
 const pdf = require('pdf-parse');
@@ -19,6 +20,25 @@ res.render('index');
 })
 app.get('/signin',(req,res)=>{
 res.render('signin');
+})
+app.get('/recommendations',(req,res)=>{
+    res.render('/recommendations');
+})
+app.get('/rec/showComp',(req,res)=>{
+    res.render('comparison');
+    
+})
+app.post('/selecteCarrier',(req,res)=>{
+
+})
+app.get('/progress',(req,res)=>{
+    res.render('progress');
+})
+app.get('prep',(req,res)=>{
+    res.render('prepPlan');
+})
+app.get('/profile', (req,res)=>{
+    res.render('profile');
 })
 
 
--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const connectDb = async () => {
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/career_guidance")
+        await mongoose.connect("mongodb://127.0.0.1:27017/career_guidance");
+        console.log("Running...")
     }catch(err){
          console.error("DB Connection Failed:", error);
     }
