ba4c10d26a284cfcab5edb65cdfa77554f9d5b13
AbhiGurjar <abhishekbainsla190@gmail.com>
2025-09-21 23:15:10 +0530

    home fixed

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 // app.use('/', routes);
 
-app.get('/home',(req,res)=>{
+app.get('/',(req,res)=>{
 res.render('index');
 })
 app.get('/signin',(req,res)=>{
